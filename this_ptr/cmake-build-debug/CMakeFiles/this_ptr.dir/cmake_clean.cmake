382dfb5d41af89784944321aecb0d1d35f918e81
wu_allin <252652905@qq.com>
2020-07-02 20:48:45 +0800

    this_ptr

---
+++ this_ptr/cmake-build-debug/CMakeFiles/this_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/this_ptr.dir/main.cpp.obj"
+  "this_ptr.pdb"
+  "this_ptr.exe"
+  "this_ptr.exe.manifest"
+  "libthis_ptr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/this_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
