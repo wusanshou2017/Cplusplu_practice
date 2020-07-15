9ff7fb267a7564fbf8fe58c81fcae4c57432f2b1
wu_allin <252652905@qq.com>
2020-07-15 18:18:27 +0800

    inline_

---
+++ inline/cmake-build-debug/CMakeFiles/inline.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inline.dir/main.cpp.obj"
+  "inline.pdb"
+  "inline.exe"
+  "inline.exe.manifest"
+  "libinline.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
